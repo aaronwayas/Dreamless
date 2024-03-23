e2ec359a9e2d94f384bfa0f47da821f5abd98a9d
Irr22 <121876059+Irr22@users.noreply.github.com>
2024-03-23 07:52:23 +0100

    Launcher.py

--- Launcher.py
+++ Launcher.py
@@ -1,203 +1,209 @@
+import threading
+import logging
 import minecraft_launcher_lib as mclib
-from tkinter import PhotoImage
+from CTkMessagebox import CTkMessagebox
 import customtkinter as ctk
-import SettingsMc
-import subprocess
-import webbrowser
-import Download
-import json
-from config import *
-# se importa la clase de las validaciones 
+import os
 from validations import Cls_validations
 
-app = ctk.CTk()
-# Obtener longitudes de la pantalla 
-scr_width = app.winfo_screenwidth()
-scr_height = app.winfo_screenheight()
-# Se le resta -150 al ancho que tenga la pantalla y -120 al alto para centrar 
-app.geometry("{}x{}".format(scr_width-150,scr_height-120))
-app.title("Dreamless")
-app._set_appearance_mode("light")
-app.config(bg="#FFFFFF")
-app.iconbitmap("assets/images/icon.ico")
 
+usr_window = os.getlogin()    
+minecraft_directory = f"C:/Users/{usr_window}/AppData/Roaming/.minecraftLauncher"
 
-# Functions
-# se crea un objeto de la clase que valida la existencia de la carpeta
-objValidatios = Cls_validations.minecraft_folder(minecraft_directory)
+logging.basicConfig(filename="error.log", level=logging.ERROR)
 
-if objValidatios == False:
-    versions_installed = ["No versions installed"]
-else:
-    versions_installed = [
-        version["id"] for version in mclib.utils.get_installed_versions(minecraft_directory)
-    ]
+# Variable global para controlar la visibilidad de la barra de progreso
+global progressbar_visible
+progressbar_visible = False
 
-def read_config_file(filename):
-    try:
-        with open(filename, "r") as file:
-            config_data = json.load(file)
-            return config_data.get("username"), config_data.get("ram")
-    except Exception as e:
-        print(f"Error reading config file: {e}")
-        return None, None
+def download_version(pantalla):
+    global progressbar_visible
+    
+    download_vrs = versions_to_install.get()
 
-def run_minecraft():
-    try:
-        mine_user, ram = read_config_file("config.json")
-        if not mine_user or not ram:
-            print("Error: Missing username or ram in config file.")
-            return
-
-        version = versions.get()
-        ram = f"-Xmx{ram}"
-
-        options = {
-            "username": mine_user,
-            "uuid": "",
-            "token": "",
-            "jvArguments": [ram, ram],
-            "launcherVersion": "0.0.2",
-        }
-        minecraft_command = mclib.command.get_minecraft_command(
-            version, minecraft_directory, options
+    # Se establece el inicio del progressbar si hay algo que descargar
+    if download_vrs:
+        # Se cambia la visibilidad de la barra de progreso y se inicia
+        progressbar_visible = True
+        progressbar.place(
+            x=28.0,
+            y=263.0,
         )
-        subprocess.run(minecraft_command)
-    except Exception as e:
-        print(f"Error running Minecraft: {e}")
+        progressbar.start()
+        
+        try:
+            mclib.install.install_minecraft_version(download_vrs, minecraft_directory)
+            logging.info("Downloaded successfully: %s", download_vrs)
+            CTkMessagebox(
+                title="Success",
+                message="Downloaded successfully",
+                icon="check",
+                bg_color="#FFFFFF",
+                text_color="#863bb4",
+                font=("SplineSans Bold", 17),
+                fg_color="#FFFFFF",
+                title_color="black",
+                button_color="#801AE5",
+                button_hover_color="#A95EFF",
+            )
+        except Exception as e:
+            logging.error("Error downloading version %s: %s", download_vrs, str(e))
+            CTkMessagebox(
+                title="Error",
+                message="Error downloading version",
+                icon="error",
+                bg_color="#FFFFFF",
+                text_color="#863bb4",
+                font=("SplineSans Bold", 17),
+                fg_color="#FFFFFF",
+                title_color="black",
+                button_color="#801AE5",
+                button_hover_color="#A95EFF",
+            )
+        
+        # Al finalizar la descarga, se detiene la barra de progreso y se oculta
+        progressbar.stop()
+        progressbar.place_forget()
+        progressbar_visible = False
+        actualizar_versiones(pantalla)
 
-def download_vanilla_or_forge(app):
+def thread_download(pantalla):
+    thd_download = threading.Thread(target=download_version, args=(pantalla,))
+    thd_download.start()
+
+def Download(pantalla):
+    global versions_to_install
+    global progressbar
     window = ctk.CTk()
-    window.geometry("200x200")
+    window.geometry("788x448")
     window.configure(bg="#FFFFFF")
     window.title("Dreamless - Download")
     window._set_appearance_mode("light")
     window.iconbitmap("assets/images/icon.ico")
-
-    download_label = ctk.CTkLabel(
-    window,
-    text="Download",
-    text_color="black",
-    bg_color="#FFFFFF",
-    font=("SplineSans", 17, "bold"),
-    cursor="hand2",
+    canvas = ctk.CTkCanvas(
+        window,
+        bg="#FFFFFF",
+        height=448,
+        width=788,
+        bd=0,
+        highlightthickness=0,
+        relief="ridge",
     )
 
-    download_label.place(relx=0.55, rely=0.063, anchor="center")
-    download_label.bind("<Button-1>", lambda event: Download.Download(app))
+    canvas.place(x=0, y=0)
+    canvas.create_text(
+        28.0,
+        14.0,
+        anchor="nw",
+        text="Dreamless",
+        fill="#140C1C",
+        font=("SplineSans Bold", 18 * -1),
+    )
 
+    canvas.create_rectangle(
+        -1.0, 46.0, 456.02740478515625, 47.0, fill="#E5E8EB", outline=""
+    )
+
+    canvas.create_text(
+        28.0,
+        97.0,
+        anchor="nw",
+        text="Select a version to install",
+        fill="#140C1C",
+        font=("SplineSans Medium", 24 * -1),
+    )
+
+    # Obtener las versiones disponibles
+    available_versions = mclib.utils.get_available_versions(minecraft_directory)
+    release_versions = [
+        version["id"] for version in available_versions if version["type"] == "release"
+    ]
+
+    versions_to_install = ctk.CTkComboBox(
+        window,
+        values=release_versions,
+        bg_color="white",
+        fg_color="#EDE8F2",
+        text_color="#140C1C",
+        width=508.0,
+        height=45,
+        font=("SplineSans Bold", 20),
+        border_color="#EDE8F2",
+        button_color="#EDE8F2",
+        dropdown_fg_color="#EDE8F2",
+        dropdown_text_color="#140C1C",
+        dropdown_hover_color="#EDE8F2",
+        dropdown_font=("SplineSans Bold", 14),
+        corner_radius=15,
+    )
+
+    versions_to_install.place(x=28.0, y=140.0)
+
+    install_button = ctk.CTkButton(
+        window,
+        command=lambda: thread_download(pantalla),
+        width=480.0,
+        text="Download",
+        fg_color="#801AE5",
+        hover_color="#A95EFF",
+        bg_color="white",
+        corner_radius=12.0,
+        text_color="white",
+        font=("SplineSans", 22),
+        height=49.93934631347656,
+    )
+    install_button.place(
+        x=28.0,
+        y=206.0,
+    )
+
+    global progressbar
+    progressbar = ctk.CTkProgressBar(
+        window,
+        width=480.0,
+        height=7,
+        orientation='horizontal',
+        corner_radius=20, 
+        fg_color="#ede8f2",
+        progress_color="#801AE5",
+        bg_color="white",
+        mode='indeterminate',
+        determinate_speed=5,
+        )
+
+    progressbar.set(0)
+
+    close_button = ctk.CTkButton(
+        window,
+        command=window.destroy,
+        width=83.0,
+        height=40.0,
+        text="Close",
+        text_color="black",
+        fg_color="#EDE8F2",
+        hover_color="#faf4ff",
+        bg_color="white",
+        corner_radius=18.0,
+        font=("SplineSans", 14, "bold"),
+    )
+    close_button.place(
+        x=660.0,
+        y=391.0,
+    )
+    window.resizable(False, False)
     window.mainloop()
 
+def actualizar_versiones(app):
+    objValidatios = Cls_validations.minecraft_folder(minecraft_directory)
 
+    if objValidatios == False:
+        versions_installed = ["No versions installed"]
+    else:
+        versions_installed = [
+            version["id"] for version in mclib.utils.get_installed_versions(minecraft_directory)
+        ]
 
-# Canvas
-
-canvas = ctk.CTkCanvas(
-    app,
-    bg="#FFFFFF",
-    height=900,
-    width=2280,
-)
-
-canvas.place(x=-2, y=-2)
-
-# Buttons and Canvas
-
-download_label = ctk.CTkLabel(
-    app,
-    text="Download",
-    text_color="black",
-    bg_color="#FFFFFF",
-    font=("SplineSans", 17, "bold"),
-    cursor="hand2",
-)
-
-download_label.place(relx=0.55, rely=0.063, anchor="center")
-
-download_label.bind("<Button-1>", lambda event: download_vanilla_or_forge(app))
-
-settings_label = ctk.CTkLabel(
-    app,
-    text="Settings",
-    text_color="black",
-    bg_color="#FFFFFF",
-    font=("SplineSans", 17, "bold"),
-    cursor="hand2",
-)
-
-settings_label.place(relx=0.65, rely=0.063, anchor="center")
-settings_label.bind("<Button-1>", lambda event: SettingsMc.Settings())
-
-buy_minecraft_label = ctk.CTkLabel(
-    app,
-    text="Buy Minecraft",
-    text_color="black",
-    bg_color="#FFFFFF",
-    font=("SplineSans", 12, "bold"),
-    cursor="hand2",
-)
-
-buy_minecraft_label.place(
-    x=667.0,
-    y=285.0,
-)
-buy_minecraft_label.bind("<Button-1>", lambda event: webbrowser.open("https://www.minecraft.net/es-es/store/minecraft-java-bedrock-edition-pc"))
-
-feedback = ctk.CTkButton(
-    app,
-    font=("SplineSans", 14),
-    text="Feedback",
-    command=lambda: webbrowser.open("https://github.com/aaronwayas/Dreamless"),
-    corner_radius=12,
-    fg_color="#801AE5",
-    bg_color="white",
-    text_color="white",
-    width=97.83000183105469,
-    height=40.0,
-    hover_color="#9d1cff",
-)
-feedback.place(
-    x=955.0,
-    y=31.0,
-)
-
-help_button = ctk.CTkButton(
-    app,
-    command=lambda: webbrowser.open("https://discord.gg/tarfQNevpf"),
-    width=20.0,
-    height=40.0,
-    text="Help",
-    text_color="black",
-    font=("SplineSans", 14, "bold"),
-    corner_radius=20,
-    fg_color="#EDE8F2",
-    bg_color="white",
-    hover_color="#faf4ff",
-)
-help_button.place(
-    x=1067.0,
-    y=31.0,
-)
-
-play_button = ctk.CTkButton(
-    app,
-    command=run_minecraft,
-    width=480.0,
-    height=54.5999755859375,
-    text="Play",
-    text_color="white",
-    bg_color="white",
-    fg_color="#801AE5",
-    corner_radius=12,
-    font=("SplineSans", 24, "bold"),
-    hover_color="#9d1cff",
-)
-play_button.place(
-    x=667.0,
-    y=235.0,
-)
-
-versions = ctk.CTkComboBox(
+    versions = ctk.CTkComboBox(
     app,
     values=versions_installed,
     bg_color="white",
@@ -213,53 +219,12 @@ versions = ctk.CTkComboBox(
     dropdown_hover_color="#EDE8F2",
     dropdown_font=("SplineSans Bold", 14),
     corner_radius=15,
-)
+    )
 
-versions.place(
-    x=667.0,
-    y=157.0,
-)
+    versions.place(
+        x=667.0,
+        y=157.0,
+    )
 
-image_image_1 = PhotoImage(file="assets/images/image_1.png")
-image_1 = canvas.create_image(331.0, 367.0, image=image_image_1)
-
-canvas.create_text(
-    240.0,
-    384.0,
-    anchor="nw",
-    text="Play Minecraft totally free",
-    fill="#FFFFFF",
-    font=("SplineSans Medium", 14 * -1, "bold"),
-)
-
-canvas.create_text(
-    156.0,
-    300.0,
-    anchor="nw",
-    text="Welcome to Minecraft",
-    fill="#FFFFFF",
-    font=("SplineSans Bold", 36 * -1, "bold"),
-)
-
-canvas.create_text(
-    142.0,
-    40.0,
-    anchor="nw",
-    text="Dreamless",
-    fill="#140C1C",
-    font=("SplineSans Bold", 19 * -1, "bold"),
-)
-
-canvas.create_rectangle(87.0, 70.0, 544.0274047851562, 71.0, fill="#E5E8EB", outline="")
-
-canvas.create_text(
-    670.0,
-    127.0,
-    anchor="nw",
-    text="Version",
-    fill="#140C1C",
-    font=("SplineSans Medium", 16 * -1),
-)
-
-app.resizable(False, False)
-app.mainloop()
+if __name__ == "__main__":
+    Download(None)
